712e47c273dc64c12740ede0899182f5cd2f1894
tuf65651 <tuf65651@temple.edu>
2019-03-01 15:03:38 -0500

    Added UI: Dummy 'Download' downloads sample file, player for sample audio file, history shows dummy JSON.

---
+++ app/static/js/ajax_history.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	var songs = [{"title": "Crowd Cheering","href": "https://sample-videos.com/audio/mp3/crowd-cheering.mp3"}, {"title": "Wave","href": "https://sample-videos.com/audio/mp3/wave.mp3"} ]
+	var history = '';
+
+	var song_index;
+	for(song_index=0; song_index < songs.length; song_index++) {
+		console.log('Begin song listing ' + song_index);
+		console.log(songs[song_index]);
+		console.log("Title is " + songs[song_index]['href']);
+		history += '<a href="';
+		history += songs[song_index]['href'] + '" ';
+		history += 'class="list-group-item">';
+		history += songs[song_index]['title'];
+		history += "</a>\n";
+	}
+
+	$('#song-history-list').append(history);
+
+	// var datastring = $.getJSON(songs, function() {
+	// 	var data = $.parseJSON(datastring)
+	// 	var table = "";
+	// 	var song;
+	// 	for (song = 0; song < data.length; song++) { 
+	// 	  history += '<a href="';
+	// 	  history += data[song]['href'] + '" ';
+	// 	  history += 'class="list-group-item">';
+	// 	  history += data[song]['title'];
+	// 	  history += "</a>\n";
+	// 	}
+
+	// 	$('#song-history-list').append(history);
+	// });
+});
